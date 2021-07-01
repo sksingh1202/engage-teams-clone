d7e06e9511d9998bc763cbd218af1fe6f50f702b
Saket Kumar Singh <saketkumar1202@gmail.com>
2021-07-01 19:45:04 +0530

    Built the front end for the video call page.

--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -4,36 +4,52 @@ import { useEffect, useRef, useState } from "react";
 // external packages
 import io from "socket.io-client";
 import styled from "styled-components";
+import { makeStyles } from "@material-ui/core/styles";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
 
 // internal components
 import { createPeer, addPeer } from "./Peer";
 import Menubar from "./Menubar";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    overflow: "hidden",
+    backgroundColor: "#202124",
+  },
+  gridList: {
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    padding: 0,
+    flexWrap: "wrap",
+    listStyle: "none",
+    overflow: "auto",
+    justifyContent: "center",
+  },
+}));
+
 const Container = styled.div`
-  padding: 20px;
-  display: flex;
-  height: 100vh;
-  width: 90%;
+  position: absolute;
+  top: 10px;
+  bottom: 10px;
+  height: 100%;
+  width: 100%;
   margin: auto;
-  flex-wrap: wrap;
+  background-color: #202124;
 `;
 
 const StyledVideo = styled.video`
-  height: 40%;
-  width: 50%;
+  position: absolute;
+  right: 1%;
+  bottom: 5%;
+  height: 16%;
+  width: 20%;
+  z-index: 1;
 `;
-
-const Video = (props) => {
-  const ref = useRef();
-
-  useEffect(() => {
-    props.peer.on("stream", (stream) => {
-      ref.current.srcObject = stream;
-    });
-  }, []);
-
-  return <StyledVideo playsInline autoPlay ref={ref} />;
-};
 
 const constraints = {
   audio: { echoCancellation: true },
@@ -43,16 +59,46 @@ const constraints = {
   },
 };
 
+const Video = (props) => {
+  const ref = useRef();
+  useEffect(() => {
+    props.peer.on("stream", (stream) => {
+      ref.current.srcObject = stream;
+    });
+  }, []);
+  return (
+    <video
+      height="100%"
+      width="100%"
+      playsInline
+      autoPlay
+      controls
+      ref={ref}
+      style={{ padding: 0, objectFit: "cover" }}
+    />
+  );
+};
+
 const Room = (props) => {
   const [peers, setPeers] = useState([]);
   const [audio, setAudio] = useState({});
   const [video, setVideo] = useState({});
+  const [height, setHeight] = useState();
+  const [screenShare, setScreenShare] = useState(false);
   const socketRef = useRef();
   const userVideo = useRef();
   const peersRef = useRef([]);
   const userStream = useRef();
 
   useEffect(() => {
+    let h = window.screen.height;
+    setHeight(h);
+    window.addEventListener("resize", () => {
+      let new_h = window.screen.height;
+      console.log(new_h);
+      setHeight(new_h);
+    });
+
     const roomID = props.match.params.roomID;
     const videoChat = async () => {
       socketRef.current = io.connect("/");
@@ -125,43 +171,52 @@ const Room = (props) => {
     props.history.push(`/`);
   };
 
-  const shareScreen = async () => {
-    try {
-      const stream = await navigator.mediaDevices.getDisplayMedia({
-        cursor: true,
-      });
-      const screenTrack = stream.getTracks()[0];
-      peersRef.current.forEach((p) => {
-        p.peer.replaceTrack(
-          video.videoTracks[0],
-          screenTrack,
-          userStream.current
-        );
-      });
-      screenTrack.onended = () =>
-        peersRef.current.forEach((p) => {
-          p.peer.replaceTrack(
-            screenTrack,
-            video.videoTracks[0],
-            userStream.current
-          );
-        });
-    } catch (e) {
-      console.log(e);
-    }
+  const calcRows = (count) => {
+    if (count <= 2) return 30;
+    if (count <= 6) return 15;
+    if (count <= 9) return 10;
+    if (count <= 16) return 7;
   };
+
+  const calcCols = (count) => {
+    if (count === 1) return 60;
+    if (count <= 4) return 30;
+    if (count <= 9) return 20;
+    if (count <= 16) return 15;
+  };
+
+  const classes = useStyles();
 
   return (
     <Container>
-      <StyledVideo muted ref={userVideo} autoPlay playsInline />
-      {peers.map((peer) => {
-        return <Video key={peer.peerID} peer={peer.peer} />;
-      })}
+      <StyledVideo controls muted ref={userVideo} autoPlay playsInline />
+      <div className={classes.root}>
+        <GridList
+          cellHeight={Math.floor(height / 35)}
+          cols={60}
+          className={classes.gridList}
+        >
+          {peers.map((peer) => (
+            <GridListTile
+              rows={calcRows(peers.length)}
+              cols={calcCols(peers.length)}
+              key={peer.peerID}
+            >
+              <Video peer={peer.peer} />
+            </GridListTile>
+          ))}
+        </GridList>
+      </div>
       <Menubar
         audio={audio}
         video={video}
+        screenShare={screenShare}
+        setAudio={setAudio}
+        setVideo={setVideo}
+        setScreenShare={setScreenShare}
         leaveRoom={leaveRoom}
-        shareScreen={shareScreen}
+        peersRef={peersRef}
+        userStream={userStream}
       />
     </Container>
   );
